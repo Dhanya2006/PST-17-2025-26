6b06f467a3e5facdaec216b0a190fc3d8bf0f0e1
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-01-19 11:07:04 +0530

    Add files via upload

---
+++ relation operators.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main {
+    public static void main(String[] args) {
+        int left = 100;
+        int right = 50;
+        System.out.println("left == right : " + (left == right));
+        System.out.println("left != right : " + (left != right));
+        System.out.println("left > right  : " + (left > right));
+        System.out.println("left < right  : " + (left < right));
+        System.out.println("left >= right : " + (left >= right));
+        System.out.println("left <= right : " + (left <= right));
+    }
+}
