295b15cd29062f6a672f590e96933250ba5a71f9
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-01-19 11:16:36 +0530

    Add files via upload

---
+++ 10-50.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (num > 10 && num < 50) {
+            System.out.println("The number is greater than 10 and less than 50.");
+        } else {
+            System.out.println("The number is not in the range 10 to 50.");
+        }
+        sc.close();
+    }
+}
---
+++ unary 2.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Post-decrement:");
+        System.out.println("a = " + a--);
+        System.out.println("Pre-decrement:");
+        System.out.println("a = " + --a); 
+    }
+}
+
