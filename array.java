0740347cac54c78438babb937e12a5e5819a0f6c
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-01-20 09:49:26 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class array
+{
+	public static void main(String[] args) {
+	    int arr[]={1,2,3,4};
+	    for(int i=0; i<=4;i++)
+	    {
+	    System.out.println(arr[i]);
+	    }
+	}
+}
