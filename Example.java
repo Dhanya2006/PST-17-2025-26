14a4e1dd2f7a09d76a8b8f008af309b9478217a6
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-05 10:21:20 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,45 @@
+package OOPSCONCEPT;
+public class Example {
+
+	public static void main(String[] args) {
+		It obj1 = new It();
+	    obj1.dhanya();
+	    obj1.ewansangel();
+	    obj1.vinothini();
+	    obj1.sridevi();
+	    Placement obj2 = new Placement();
+	    obj2.Training();
+	    obj2.Training1();
+	    obj2.Training2();
+	    obj2.Training3();
+	
+	}
+}
+class It{
+	 public static void dhanya(){
+	        System.out.println("dhanya");
+	 }
+	 public static void ewansangel() {
+	        System.out.println("dhanya");
+	 }
+	 public static void vinothini() {
+	        System.out.println("vinothini");
+	 }
+	 public static void sridevi() {
+	        System.out.println("sridevi");
+	 }
+    }
+class Placement{
+ public static void Training(){
+     System.out.println("dhanya learn java");
+ }
+ public static void Training1(){
+     System.out.println("ewansangel learn html");
+ }
+ public static void Training2(){
+     System.out.println("vinothini learn css");
+ }
+ public static void Training3(){
+     System.out.println("sridevi learn html&css");
+ }
+	}
---
+++ Polymorphism.java
@@ -0,0 +1,12 @@
+package OOPSCONCEPT;
+
+public class Polymorphism {
+
+	public static void main(String[] args) {
+		System.out.println(example());
+	}
+	public static int example(){
+        int a = 10;
+	    return a ;
+	}
+	}
