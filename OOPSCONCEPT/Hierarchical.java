dd86fbcb58af80e8cc2107c11e277d566ba37708
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-13 09:19:05 +0530

    Add files via upload

---
+++ OOPSCONCEPT/Hierarchical.java
@@ -0,0 +1,20 @@
+package OOPSCONCEPT;
+
+public class Hierarchical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class A {
+	
+
+}
+class B extends A{
+	
+}
+class C extends A{
+	
+}
