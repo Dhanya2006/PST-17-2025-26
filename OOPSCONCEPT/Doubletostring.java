ff8a88d34963de1f1c96dfb77cea5a404e715145
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-13 09:17:17 +0530

    Add files via upload

---
+++ OOPSCONCEPT/Doubletostring.java
@@ -0,0 +1,14 @@
+package OOPSCONCEPT;
+import java.util.Scanner;
+public class Doubletostring {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter a values:");
+		double a = sc.nextDouble();
+		int num=0;
+		
+
+	}
+
+}
