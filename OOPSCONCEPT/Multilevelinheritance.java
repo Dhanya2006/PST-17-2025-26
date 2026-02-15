153d4d2baf941924ee2951b5e58625b6130a3f0c
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-15 18:54:46 +0530

    Add files via upload

---
+++ OOPSCONCEPT/Multilevelinheritance.java
@@ -0,0 +1,22 @@
+package OOPSCONCEPT;
+
+public class Multilevelinheritance {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+class A{
+	
+}
+class B extends A{
+	
+}
+class C extends B{
+	
+}
+class D extends C{
+	
+}
