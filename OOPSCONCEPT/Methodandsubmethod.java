03697a82acc35ecd0b5e9239c1ec4f833c581046
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-15 18:55:17 +0530

    Add files via upload

---
+++ OOPSCONCEPT/Methodandsubmethod.java
@@ -0,0 +1,23 @@
+package OOPSCONCEPT;
+
+public class Methodandsubmethod {
+
+	public static void main(String[] args) {
+		ewansangel();
+	}
+	public static void ewansangel(){
+	    System.out.println("Ewans angel is crazy");
+	    vinothini();
+	}
+	public static void vinothini(){
+	    System.out.println("Vinothini is crying girl ");
+	    sridevi();
+	}
+	public static void sridevi(){
+	    System.out.println("Sridevi is rowdypompala");
+	    manjima();
+	}
+	public static void manjima(){
+	    System.out.println("Manjima is silent girl");
+	}
+}
