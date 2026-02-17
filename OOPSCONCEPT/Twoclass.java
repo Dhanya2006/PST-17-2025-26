0c2da975fbbef4c8f2018f86195540fbfbe96aac
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-17 22:21:29 +0530

    Add files via upload

---
+++ OOPSCONCEPT/Twoclass.java
@@ -0,0 +1,23 @@
+package OOPSCONCEPT;
+
+public class Twoclass {
+
+	public static void main(String[] args) {
+		classB obj = new classB();
+		obj.cse();
+	}
+}
+		
+ class classA {
+    void cse() {
+    	
+	        System.out.println("dhanya");
+	 }
+    }	
+     
+ class classB extends  classA {
+    void it() {
+    	System.out.println("ewansangel");	
+    }
+    }
+ 
