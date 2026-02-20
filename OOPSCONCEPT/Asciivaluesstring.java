1a251c544e867650d013a4c0782f1e9b527a62e6
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-20 22:40:09 +0530

    Add files via upload

---
+++ OOPSCONCEPT/Asciivaluesstring.java
@@ -0,0 +1,23 @@
+package ASCIIFUNCTION;
+import java.util.Scanner;
+public class Asciivaluesstring {
+
+	public static void main(String[] args) {
+		String str = method();
+        method1(str);            
+    }
+    static String method() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        return sc.nextLine();
+    }
+    static void method1(String str) {
+        for(int i = 0; i < str.length(); i++) {
+            char a = str.charAt(i);
+            int ascii = a;
+            System.out.println(a + " = " + ascii);
+        }
+
+	}
+
+}
