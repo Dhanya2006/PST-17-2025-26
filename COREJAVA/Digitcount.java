9ca21ab6cd150b59640c6b403da9daa548985584
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-02-21 14:21:56 +0530

    Add files via upload

---
+++ COREJAVA/Digitcount.java
@@ -0,0 +1,20 @@
+package PST;
+import java.util.Scanner;
+public class Digitcount {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the numbers:");
+		int num = sc.nextInt();
+	        int count = 0;
+	        if (num == 0) {
+	            count = 1;
+	        } else {
+	            while (num != 0) {
+	                count++;
+	                num = num / 10;
+	            }
+	        }
+	        System.out.println("Number of digits = " + count);
+	        sc.close();
+	}
+}
